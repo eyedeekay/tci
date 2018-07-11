4490146ea828af7dfea00cbc07e26448d0b5fb6f
idk <problemsolver@openmailbox.org>
2018-07-10 22:45:24 -0400

    add stub for log command

---
+++ log.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/henrikhodne/tci/travis"
+	"strings"
+)
+
+func init() {
+	cmds["log"] = cmd{log, "", "display information about the latest build"}
+	cmdHelp["log"] = `Shows a summary of the latest build.
+`
+}
+
+func log() {
+	slug := detectSlug()
+	client := travis.NewClient()
+
+	repoResp, _ := client.GetRepository(slug)
+	if repoResp == (travis.RepositoryResponse{}) {
+		println("Couldn't find repository")
+		return
+	}
+
+	buildResp, _ := client.GetBuild(repoResp.Repository.LastBuildID)
+	if buildResp == (travis.BuildResponse{}) {
+		println("Couldn't find build.")
+		return
+	}
+
+	build := buildResp.Build
+	commit := buildResp.Commit
+
+	fmt.Printf(bold("Build #%s: %s\n"), build.Number, strings.Split(commit.Message, "\n")[0])
+	printInfo("State", build.State)
+	if build.PullRequest {
+		printInfo("Type", "pull request")
+	} else {
+		printInfo("Type", "push")
+	}
+	printInfo("Branch", commit.Branch)
+	printInfo("Compare URL", commit.CompareURL)
+	printInfo("Duration", formatDuration(build.Duration))
+	printInfo("Started", formatTime(build.StartedAt))
+	printInfo("Finished", formatTime(build.FinishedAt))
+}
